f53cda3f89bc944eb1102eedc9ed2845bf79000d
Danny <danny.costelloe@gmail.com>
2021-03-19 15:16:17 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const newTaskPriority = document.getElementById("new")
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("this works")
 });
+
+
+const submitBtn = document.querySelector("input")
+let newTaskForm = document.getElementById("create-task-form");
+
+
+newTaskForm.addEventListener("click", function(){
+  event.preventDefault()
+  console.log("Button works!")
+})
+
+const newTaskDescription = document.getElementById("new-task-description");
+
+function addNewItem(){
+  event.preventDefault();
+}
